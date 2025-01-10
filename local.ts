3168e5b474b9553b65a02f645be0260b14a2d3b6
Dinhero21 <dinhero21@dinhero21.dev>
2025-01-10 11:04:10 -0300

    wait music (literally)

--- local.ts
+++ local.ts
@@ -1,6 +1,9 @@
 import { createChannel } from '../../worker/parent.js';
 import chat from '../ddg.chat/local.js';
+import { pose } from '../ddg.pose/local.js';
+import { sound } from '../ddg.sound/local.js';
 import proxy from '../internal.proxy/local.js';
+import { RECONNECT_SOUND as MUSIC_RESOURCE } from './settings.js';
 import type { LocalMessage } from './shared.js';
 
 export const channel = createChannel<LocalMessage, never>(
@@ -8,7 +11,19 @@ export const channel = createChannel<LocalMessage, never>(
 );
 
 export function reconnect(host?: string, port?: number): void {
+  if (MUSIC_RESOURCE !== undefined)
+    sound.playSound(
+      { resource: MUSIC_RESOURCE, range: undefined },
+      undefined,
+      pose.position.x,
+      pose.position.y,
+      pose.position.z,
+    );
+
   channel.write([host, port]);
+
+  // don't run anything here, worker will be terminated at some point
+  // (btw, just by adding a console.log here, I got a v8 null pointer dereference error, spooky!)
 }
 
 proxy.downstream.on('kick_disconnect', (packet) => {
--- settings.ts
+++ settings.ts
@@ -1,2 +1,4 @@
 export const RECONNECT_SECONDS: number = 5;
 export const TERMINATE_WORKER_IMMEDIATELY: boolean = true;
+
+export const RECONNECT_SOUND: string | undefined = 'minecraft:music_disc.wait';
